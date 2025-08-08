f89790b6459ecbb1615289cee450d6605ef0c434
Isaac <1812924685@qq.com>
2025-08-08 14:25:42 +0800

    breeze: dji/rm_tyepc fix tab into space

--- samples/boards/rm_typec/buzzer/src/main.c
+++ samples/boards/rm_typec/buzzer/src/main.c
@@ -91,20 +91,20 @@
 
 // Note and duration structure
 struct note_duration {
-	int note;     /* hz */
-	int duration; /* msec */
+    int note;     /* hz */
+    int duration; /* msec */
 };
 
 // Song enumeration
 enum song_type {
-	SONG_TWINKLE_STAR,
-	SONG_HAPPY_BIRTHDAY,
-	SONG_BEEP_TEST,
-	SONG_GAME_OF_THRONES,
-	SONG_SUPER_MARIO,
-	SONG_MAX_VERSTAPPEN,
-	SONG_PIRATES_CARIBBEAN,
-	SONG_COUNT
+    SONG_TWINKLE_STAR,
+    SONG_HAPPY_BIRTHDAY,
+    SONG_BEEP_TEST,
+    SONG_GAME_OF_THRONES,
+    SONG_SUPER_MARIO,
+    SONG_MAX_VERSTAPPEN,
+    SONG_PIRATES_CARIBBEAN,
+    SONG_COUNT
 };
 
 // Get PWM device and channel from buzzer0 alias
@@ -122,251 +122,251 @@ static const struct device *buzzer_dev;
 
 // Song definitions
 static const struct note_duration twinkle_star[] = {
-	{NOTE_C4, 500}, {NOTE_C4, 500}, {NOTE_G4, 500}, {NOTE_G4, 500},
-	{NOTE_A4, 500}, {NOTE_A4, 500}, {NOTE_G4, 1000},
-	{NOTE_F4, 500}, {NOTE_F4, 500}, {NOTE_E4, 500}, {NOTE_E4, 500},
-	{NOTE_D4, 500}, {NOTE_D4, 500}, {NOTE_C4, 1000},
+    {NOTE_C4, 500}, {NOTE_C4, 500}, {NOTE_G4, 500}, {NOTE_G4, 500},
+    {NOTE_A4, 500}, {NOTE_A4, 500}, {NOTE_G4, 1000},
+    {NOTE_F4, 500}, {NOTE_F4, 500}, {NOTE_E4, 500}, {NOTE_E4, 500},
+    {NOTE_D4, 500}, {NOTE_D4, 500}, {NOTE_C4, 1000},
 };
 
 static const struct note_duration happy_birthday[] = {
-	{NOTE_C4, 250}, {NOTE_C4, 250}, {NOTE_D4, 500}, {NOTE_C4, 500},
-	{NOTE_F4, 500}, {NOTE_E4, 1000},
-	{NOTE_C4, 250}, {NOTE_C4, 250}, {NOTE_D4, 500}, {NOTE_C4, 500},
-	{NOTE_G4, 500}, {NOTE_F4, 1000},
+    {NOTE_C4, 250}, {NOTE_C4, 250}, {NOTE_D4, 500}, {NOTE_C4, 500},
+    {NOTE_F4, 500}, {NOTE_E4, 1000},
+    {NOTE_C4, 250}, {NOTE_C4, 250}, {NOTE_D4, 500}, {NOTE_C4, 500},
+    {NOTE_G4, 500}, {NOTE_F4, 1000},
 };
 
 static const struct note_duration beep_test[] = {
-	{NOTE_A4, 200}, {NOTE_REST, 100}, {NOTE_A4, 200}, {NOTE_REST, 100},
-	{NOTE_A4, 200}, {NOTE_REST, 500},
+    {NOTE_A4, 200}, {NOTE_REST, 100}, {NOTE_A4, 200}, {NOTE_REST, 100},
+    {NOTE_A4, 200}, {NOTE_REST, 500},
 };
 
 // Game of Thrones theme song - Enhanced with extended note range
 static const struct note_duration game_of_thrones[] = {
-	// Opening melody - lower octave for dramatic effect
-	{NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_DS3, 250}, {NOTE_F3, 250},
-	{NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_DS3, 250}, {NOTE_F3, 250},
-	{NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_DS3, 250}, {NOTE_F3, 250},
-	{NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_E3, 250}, {NOTE_F3, 250},
-	
-	// Rising melody with octave jump
-	{NOTE_G3, 500}, {NOTE_C4, 500}, {NOTE_DS4, 250}, {NOTE_F4, 250},
-	{NOTE_G4, 500}, {NOTE_C4, 500}, {NOTE_DS4, 250}, {NOTE_F4, 250},
-	
-	// Climax with higher notes
-	{NOTE_AS4, 250}, {NOTE_C5, 250}, {NOTE_D5, 500}, {NOTE_G4, 500},
-	{NOTE_AS4, 250}, {NOTE_C5, 250}, {NOTE_D5, 750}, {NOTE_REST, 250},
-	
-	// Second theme - more complex harmony
-	{NOTE_F4, 500}, {NOTE_AS3, 500}, {NOTE_C4, 500}, {NOTE_D4, 500},
-	{NOTE_DS4, 500}, {NOTE_F4, 500}, {NOTE_G4, 1000},
-	
-	// Epic finale with wide range
-	{NOTE_C3, 250}, {NOTE_G3, 250}, {NOTE_C4, 250}, {NOTE_G4, 250},
-	{NOTE_C5, 500}, {NOTE_AS4, 500}, {NOTE_G4, 500}, {NOTE_F4, 500},
-	{NOTE_DS4, 500}, {NOTE_D4, 500}, {NOTE_C4, 1000}, {NOTE_REST, 500},
-	
-	// Final dramatic notes
-	{NOTE_G2, 1000}, {NOTE_C3, 1500}, {NOTE_REST, 1000},
+    // Opening melody - lower octave for dramatic effect
+    {NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_DS3, 250}, {NOTE_F3, 250},
+    {NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_DS3, 250}, {NOTE_F3, 250},
+    {NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_DS3, 250}, {NOTE_F3, 250},
+    {NOTE_G3, 500}, {NOTE_C3, 500}, {NOTE_E3, 250}, {NOTE_F3, 250},
+    
+    // Rising melody with octave jump
+    {NOTE_G3, 500}, {NOTE_C4, 500}, {NOTE_DS4, 250}, {NOTE_F4, 250},
+    {NOTE_G4, 500}, {NOTE_C4, 500}, {NOTE_DS4, 250}, {NOTE_F4, 250},
+    
+    // Climax with higher notes
+    {NOTE_AS4, 250}, {NOTE_C5, 250}, {NOTE_D5, 500}, {NOTE_G4, 500},
+    {NOTE_AS4, 250}, {NOTE_C5, 250}, {NOTE_D5, 750}, {NOTE_REST, 250},
+    
+    // Second theme - more complex harmony
+    {NOTE_F4, 500}, {NOTE_AS3, 500}, {NOTE_C4, 500}, {NOTE_D4, 500},
+    {NOTE_DS4, 500}, {NOTE_F4, 500}, {NOTE_G4, 1000},
+    
+    // Epic finale with wide range
+    {NOTE_C3, 250}, {NOTE_G3, 250}, {NOTE_C4, 250}, {NOTE_G4, 250},
+    {NOTE_C5, 500}, {NOTE_AS4, 500}, {NOTE_G4, 500}, {NOTE_F4, 500},
+    {NOTE_DS4, 500}, {NOTE_D4, 500}, {NOTE_C4, 1000}, {NOTE_REST, 500},
+    
+    // Final dramatic notes
+    {NOTE_G2, 1000}, {NOTE_C3, 1500}, {NOTE_REST, 1000},
 };
 
 // Super Mario Bros Theme Song
 static const struct note_duration super_mario[] = {
-	// Main theme opening
-	{NOTE_E5, 150}, {NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_E5, 150},
-	{NOTE_REST, 150}, {NOTE_C5, 150}, {NOTE_E5, 150}, {NOTE_REST, 150},
-	{NOTE_G5, 150}, {NOTE_REST, 450}, {NOTE_G4, 150}, {NOTE_REST, 450},
-	
-	// First verse
-	{NOTE_C5, 150}, {NOTE_REST, 300}, {NOTE_G4, 150}, {NOTE_REST, 300},
-	{NOTE_E4, 150}, {NOTE_REST, 300}, {NOTE_A4, 150}, {NOTE_REST, 150},
-	{NOTE_B4, 150}, {NOTE_REST, 150}, {NOTE_AS4, 150}, {NOTE_A4, 150},
-	{NOTE_REST, 150},
-	
-	// Second part
-	{NOTE_G4, 200}, {NOTE_E5, 200}, {NOTE_G5, 200}, {NOTE_A5, 150},
-	{NOTE_REST, 150}, {NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_REST, 150},
-	{NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_C5, 150}, {NOTE_D5, 150},
-	{NOTE_B4, 150}, {NOTE_REST, 300},
-	
-	// Repeat opening
-	{NOTE_C5, 150}, {NOTE_REST, 300}, {NOTE_G4, 150}, {NOTE_REST, 300},
-	{NOTE_E4, 150}, {NOTE_REST, 300}, {NOTE_A4, 150}, {NOTE_REST, 150},
-	{NOTE_B4, 150}, {NOTE_REST, 150}, {NOTE_AS4, 150}, {NOTE_A4, 150},
-	{NOTE_REST, 150},
-	
-	// Final section
-	{NOTE_G4, 200}, {NOTE_E5, 200}, {NOTE_G5, 200}, {NOTE_A5, 150},
-	{NOTE_REST, 150}, {NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_REST, 150},
-	{NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_C5, 150}, {NOTE_D5, 150},
-	{NOTE_B4, 150}, {NOTE_REST, 300},
-	
-	// Ending
-	{NOTE_G5, 150}, {NOTE_FS5, 150}, {NOTE_F5, 150}, {NOTE_DS5, 150},
-	{NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_GS4, 150}, {NOTE_A4, 150},
-	{NOTE_C5, 150}, {NOTE_REST, 150}, {NOTE_A4, 150}, {NOTE_C5, 150},
-	{NOTE_D5, 150}, {NOTE_REST, 300},
-	
-	// Classic Mario ending - matching the sheet music (1 1)
-	{NOTE_C6, 300}, {NOTE_C6, 600},
+    // Main theme opening
+    {NOTE_E5, 150}, {NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_E5, 150},
+    {NOTE_REST, 150}, {NOTE_C5, 150}, {NOTE_E5, 150}, {NOTE_REST, 150},
+    {NOTE_G5, 150}, {NOTE_REST, 450}, {NOTE_G4, 150}, {NOTE_REST, 450},
+    
+    // First verse
+    {NOTE_C5, 150}, {NOTE_REST, 300}, {NOTE_G4, 150}, {NOTE_REST, 300},
+    {NOTE_E4, 150}, {NOTE_REST, 300}, {NOTE_A4, 150}, {NOTE_REST, 150},
+    {NOTE_B4, 150}, {NOTE_REST, 150}, {NOTE_AS4, 150}, {NOTE_A4, 150},
+    {NOTE_REST, 150},
+    
+    // Second part
+    {NOTE_G4, 200}, {NOTE_E5, 200}, {NOTE_G5, 200}, {NOTE_A5, 150},
+    {NOTE_REST, 150}, {NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_REST, 150},
+    {NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_C5, 150}, {NOTE_D5, 150},
+    {NOTE_B4, 150}, {NOTE_REST, 300},
+    
+    // Repeat opening
+    {NOTE_C5, 150}, {NOTE_REST, 300}, {NOTE_G4, 150}, {NOTE_REST, 300},
+    {NOTE_E4, 150}, {NOTE_REST, 300}, {NOTE_A4, 150}, {NOTE_REST, 150},
+    {NOTE_B4, 150}, {NOTE_REST, 150}, {NOTE_AS4, 150}, {NOTE_A4, 150},
+    {NOTE_REST, 150},
+    
+    // Final section
+    {NOTE_G4, 200}, {NOTE_E5, 200}, {NOTE_G5, 200}, {NOTE_A5, 150},
+    {NOTE_REST, 150}, {NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_REST, 150},
+    {NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_C5, 150}, {NOTE_D5, 150},
+    {NOTE_B4, 150}, {NOTE_REST, 300},
+    
+    // Ending
+    {NOTE_G5, 150}, {NOTE_FS5, 150}, {NOTE_F5, 150}, {NOTE_DS5, 150},
+    {NOTE_E5, 150}, {NOTE_REST, 150}, {NOTE_GS4, 150}, {NOTE_A4, 150},
+    {NOTE_C5, 150}, {NOTE_REST, 150}, {NOTE_A4, 150}, {NOTE_C5, 150},
+    {NOTE_D5, 150}, {NOTE_REST, 300},
+    
+    // Classic Mario ending - matching the sheet music (1 1)
+    {NOTE_C6, 300}, {NOTE_C6, 600},
 };
 
 // Max Verstappen Theme Song - "dudududu Max Verstappen"
 static const struct note_duration max_verstappen[] = {
-	// "du du du du du"
-	{NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_D6, 400},
-	{NOTE_REST, 200},
-	
-	// "Max Ver-stap-pen"
-	{NOTE_C6, 300}, {NOTE_C6, 300}, {NOTE_AS5, 300}, {NOTE_A5, 600},
-	{NOTE_REST, 400},
-	
-	// "du du du du du"
-	{NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_D6, 400},
-	{NOTE_REST, 200},
-	
-	// "Max Ver-stap-pen"
-	{NOTE_C6, 300}, {NOTE_C6, 300}, {NOTE_AS5, 300}, {NOTE_A5, 600},
-	{NOTE_REST, 400},
+    // "du du du du du"
+    {NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_D6, 400},
+    {NOTE_REST, 200},
+    
+    // "Max Ver-stap-pen"
+    {NOTE_C6, 300}, {NOTE_C6, 300}, {NOTE_AS5, 300}, {NOTE_A5, 600},
+    {NOTE_REST, 400},
+    
+    // "du du du du du"
+    {NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_G5, 200}, {NOTE_D6, 400},
+    {NOTE_REST, 200},
+    
+    // "Max Ver-stap-pen"
+    {NOTE_C6, 300}, {NOTE_C6, 300}, {NOTE_AS5, 300}, {NOTE_A5, 600},
+    {NOTE_REST, 400},
 };
 
 // Pirates of the Caribbean - "He's a Pirate" Theme
 static const struct note_duration pirates_caribbean[] = {
-	// Main theme opening
-	{NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_D5, 200},
-	{NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_F5, 200},
-	{NOTE_F5, 200}, {NOTE_G5, 200}, {NOTE_E5, 200}, {NOTE_E5, 200},
-	{NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_D5, 400},
-	
-	// Second phrase
-	{NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_D5, 200},
-	{NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_F5, 200},
-	{NOTE_F5, 200}, {NOTE_G5, 200}, {NOTE_E5, 200}, {NOTE_E5, 200},
-	{NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_D5, 400},
-	
-	// Bridge section
-	{NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_F5, 200},
-	{NOTE_G5, 200}, {NOTE_A5, 200}, {NOTE_D5, 400}, {NOTE_REST, 200},
-	{NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_F5, 200},
-	{NOTE_G5, 200}, {NOTE_A5, 200}, {NOTE_D5, 400},
-	
-	// Climactic section
-	{NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_G5, 200},
-	{NOTE_A5, 200}, {NOTE_AS5, 200}, {NOTE_A5, 200}, {NOTE_G5, 200},
-	{NOTE_F5, 200}, {NOTE_E5, 200}, {NOTE_D5, 200}, {NOTE_C5, 200},
-	{NOTE_D5, 400}, {NOTE_REST, 200},
-	
-	// Return to main theme
-	{NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_D5, 200},
-	{NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_F5, 200},
-	{NOTE_F5, 200}, {NOTE_G5, 200}, {NOTE_E5, 200}, {NOTE_E5, 200},
-	{NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_D5, 400},
-	
-	// Fast section
-	{NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_A5, 150}, {NOTE_AS5, 150},
-	{NOTE_A5, 150}, {NOTE_G5, 150}, {NOTE_F5, 150}, {NOTE_E5, 150},
-	{NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_A5, 150}, {NOTE_G5, 150},
-	{NOTE_F5, 150}, {NOTE_E5, 150}, {NOTE_D5, 300},
-	
-	// Adventure melody
-	{NOTE_A5, 200}, {NOTE_G5, 200}, {NOTE_F5, 200}, {NOTE_E5, 200},
-	{NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_AS4, 200}, {NOTE_A4, 200},
-	{NOTE_D5, 400}, {NOTE_F5, 400}, {NOTE_A5, 600},
-	
-	// Final triumphant section
-	{NOTE_D6, 200}, {NOTE_C6, 200}, {NOTE_AS5, 200}, {NOTE_A5, 200},
-	{NOTE_G5, 200}, {NOTE_F5, 200}, {NOTE_E5, 200}, {NOTE_D5, 200},
-	{NOTE_A5, 300}, {NOTE_F5, 300}, {NOTE_D5, 600},
-	
-	// Ending
-	{NOTE_A4, 200}, {NOTE_D5, 200}, {NOTE_F5, 200}, {NOTE_A5, 400},
-	{NOTE_D6, 800}, {NOTE_REST, 400},
+    // Main theme opening
+    {NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_D5, 200},
+    {NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_F5, 200},
+    {NOTE_F5, 200}, {NOTE_G5, 200}, {NOTE_E5, 200}, {NOTE_E5, 200},
+    {NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_D5, 400},
+    
+    // Second phrase
+    {NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_D5, 200},
+    {NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_F5, 200},
+    {NOTE_F5, 200}, {NOTE_G5, 200}, {NOTE_E5, 200}, {NOTE_E5, 200},
+    {NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_D5, 400},
+    
+    // Bridge section
+    {NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_F5, 200},
+    {NOTE_G5, 200}, {NOTE_A5, 200}, {NOTE_D5, 400}, {NOTE_REST, 200},
+    {NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_F5, 200},
+    {NOTE_G5, 200}, {NOTE_A5, 200}, {NOTE_D5, 400},
+    
+    // Climactic section
+    {NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_G5, 200},
+    {NOTE_A5, 200}, {NOTE_AS5, 200}, {NOTE_A5, 200}, {NOTE_G5, 200},
+    {NOTE_F5, 200}, {NOTE_E5, 200}, {NOTE_D5, 200}, {NOTE_C5, 200},
+    {NOTE_D5, 400}, {NOTE_REST, 200},
+    
+    // Return to main theme
+    {NOTE_A4, 200}, {NOTE_C5, 200}, {NOTE_D5, 200}, {NOTE_D5, 200},
+    {NOTE_D5, 200}, {NOTE_E5, 200}, {NOTE_F5, 200}, {NOTE_F5, 200},
+    {NOTE_F5, 200}, {NOTE_G5, 200}, {NOTE_E5, 200}, {NOTE_E5, 200},
+    {NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_D5, 400},
+    
+    // Fast section
+    {NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_A5, 150}, {NOTE_AS5, 150},
+    {NOTE_A5, 150}, {NOTE_G5, 150}, {NOTE_F5, 150}, {NOTE_E5, 150},
+    {NOTE_F5, 150}, {NOTE_G5, 150}, {NOTE_A5, 150}, {NOTE_G5, 150},
+    {NOTE_F5, 150}, {NOTE_E5, 150}, {NOTE_D5, 300},
+    
+    // Adventure melody
+    {NOTE_A5, 200}, {NOTE_G5, 200}, {NOTE_F5, 200}, {NOTE_E5, 200},
+    {NOTE_D5, 200}, {NOTE_C5, 200}, {NOTE_AS4, 200}, {NOTE_A4, 200},
+    {NOTE_D5, 400}, {NOTE_F5, 400}, {NOTE_A5, 600},
+    
+    // Final triumphant section
+    {NOTE_D6, 200}, {NOTE_C6, 200}, {NOTE_AS5, 200}, {NOTE_A5, 200},
+    {NOTE_G5, 200}, {NOTE_F5, 200}, {NOTE_E5, 200}, {NOTE_D5, 200},
+    {NOTE_A5, 300}, {NOTE_F5, 300}, {NOTE_D5, 600},
+    
+    // Ending
+    {NOTE_A4, 200}, {NOTE_D5, 200}, {NOTE_F5, 200}, {NOTE_A5, 400},
+    {NOTE_D6, 800}, {NOTE_REST, 400},
 };
 
 // Song table
 static const struct {
-	const struct note_duration *notes;
-	size_t count;
+    const struct note_duration *notes;
+    size_t count;
 } songs[SONG_COUNT] = {
-	[SONG_TWINKLE_STAR] = {twinkle_star, ARRAY_SIZE(twinkle_star)},
-	[SONG_HAPPY_BIRTHDAY] = {happy_birthday, ARRAY_SIZE(happy_birthday)},
-	[SONG_BEEP_TEST] = {beep_test, ARRAY_SIZE(beep_test)},
-	[SONG_GAME_OF_THRONES] = {game_of_thrones, ARRAY_SIZE(game_of_thrones)},
-	[SONG_SUPER_MARIO] = {super_mario, ARRAY_SIZE(super_mario)},
-	[SONG_MAX_VERSTAPPEN] = {max_verstappen, ARRAY_SIZE(max_verstappen)},
-	[SONG_PIRATES_CARIBBEAN] = {pirates_caribbean, ARRAY_SIZE(pirates_caribbean)},
+    [SONG_TWINKLE_STAR] = {twinkle_star, ARRAY_SIZE(twinkle_star)},
+    [SONG_HAPPY_BIRTHDAY] = {happy_birthday, ARRAY_SIZE(happy_birthday)},
+    [SONG_BEEP_TEST] = {beep_test, ARRAY_SIZE(beep_test)},
+    [SONG_GAME_OF_THRONES] = {game_of_thrones, ARRAY_SIZE(game_of_thrones)},
+    [SONG_SUPER_MARIO] = {super_mario, ARRAY_SIZE(super_mario)},
+    [SONG_MAX_VERSTAPPEN] = {max_verstappen, ARRAY_SIZE(max_verstappen)},
+    [SONG_PIRATES_CARIBBEAN] = {pirates_caribbean, ARRAY_SIZE(pirates_caribbean)},
 };
 
 // Play a single note
 static void play_note(int frequency, int duration_ms)
 {
-	if (frequency == NOTE_REST) {
-		// Rest - turn off buzzer
-		pwm_set(buzzer_dev, BUZZER_PWM_CHANNEL, 0, 0, 0);
-	} else {
-		// Calculate period in microseconds
-		uint32_t period_usec = 1000000 / frequency;
-		// Set 50% duty cycle
-		pwm_set(buzzer_dev, BUZZER_PWM_CHANNEL, PWM_USEC(period_usec), PWM_USEC(period_usec / 2), 0);
-	}
-	
-	k_msleep(duration_ms);
-	
-	// Turn off buzzer after note
-	pwm_set(buzzer_dev, BUZZER_PWM_CHANNEL, 0, 0, 0);
-	k_msleep(50); // Small gap between notes
+    if (frequency == NOTE_REST) {
+        // Rest - turn off buzzer
+        pwm_set(buzzer_dev, BUZZER_PWM_CHANNEL, 0, 0, 0);
+    } else {
+        // Calculate period in microseconds
+        uint32_t period_usec = 1000000 / frequency;
+        // Set 50% duty cycle
+        pwm_set(buzzer_dev, BUZZER_PWM_CHANNEL, PWM_USEC(period_usec), PWM_USEC(period_usec / 2), 0);
+    }
+    
+    k_msleep(duration_ms);
+    
+    // Turn off buzzer after note
+    pwm_set(buzzer_dev, BUZZER_PWM_CHANNEL, 0, 0, 0);
+    k_msleep(50); // Small gap between notes
 }
 
 // Play song interface function
 int play_song(enum song_type song, double speed_multiplier, double pitch_multiplier)
 {
-	if (song >= SONG_COUNT) {
-		printk("Error: Invalid song type\n");
-		return -1;
-	}
-	
-	if (!device_is_ready(buzzer_dev)) {
-		printk("Error: PWM device not ready\n");
-		return -1;
-	}
-	
-	if (speed_multiplier <= 0.0) {
-		printk("Error: Invalid speed multiplier\n");
-		return -1;
-	}
-	
-	if (pitch_multiplier <= 0.0) {
-		printk("Error: Invalid pitch multiplier\n");
-		return -1;
-	}
-	
-	printk("Playing song %d at %.2fx speed and %.2fx pitch...\n", song, speed_multiplier, pitch_multiplier);
-	
-	const struct note_duration *notes = songs[song].notes;
-	size_t note_count = songs[song].count;
-	
-	for (size_t i = 0; i < note_count; i++) {
-		int adjusted_duration = (int)(notes[i].duration / speed_multiplier);
-		int adjusted_frequency = (notes[i].note == NOTE_REST) ? NOTE_REST : (int)(notes[i].note * pitch_multiplier);
-		play_note(adjusted_frequency, adjusted_duration);
-	}
-	
-	printk("Song finished\n");
-	return 0;
+    if (song >= SONG_COUNT) {
+        printk("Error: Invalid song type\n");
+        return -1;
+    }
+    
+    if (!device_is_ready(buzzer_dev)) {
+        printk("Error: PWM device not ready\n");
+        return -1;
+    }
+    
+    if (speed_multiplier <= 0.0) {
+        printk("Error: Invalid speed multiplier\n");
+        return -1;
+    }
+    
+    if (pitch_multiplier <= 0.0) {
+        printk("Error: Invalid pitch multiplier\n");
+        return -1;
+    }
+    
+    printk("Playing song %d at %.2fx speed and %.2fx pitch...\n", song, speed_multiplier, pitch_multiplier);
+    
+    const struct note_duration *notes = songs[song].notes;
+    size_t note_count = songs[song].count;
+    
+    for (size_t i = 0; i < note_count; i++) {
+        int adjusted_duration = (int)(notes[i].duration / speed_multiplier);
+        int adjusted_frequency = (notes[i].note == NOTE_REST) ? NOTE_REST : (int)(notes[i].note * pitch_multiplier);
+        play_note(adjusted_frequency, adjusted_duration);
+    }
+    
+    printk("Song finished\n");
+    return 0;
 }
 
 int main(void)
 {
-	buzzer_dev = DEVICE_DT_GET(BUZZER_PWM_CTLR);
+    buzzer_dev = DEVICE_DT_GET(BUZZER_PWM_CTLR);
 
-	if (!device_is_ready(buzzer_dev)) {
-		printk("Error: PWM device not ready\n");
-		return -1;
-	}
+    if (!device_is_ready(buzzer_dev)) {
+        printk("Error: PWM device not ready\n");
+        return -1;
+    }
 
-	printk("Music player started (PWM channel %d)\n", BUZZER_PWM_CHANNEL);
+    printk("Music player started (PWM channel %d)\n", BUZZER_PWM_CHANNEL);
 
-	// Play Pirates of the Caribbean theme at 1.25x speed and 2.0x pitch (one octave higher)
-	play_song(SONG_PIRATES_CARIBBEAN, 1.25, 2.0);
+    // Play Pirates of the Caribbean theme at 1.25x speed and 2.0x pitch (one octave higher)
+    play_song(SONG_PIRATES_CARIBBEAN, 1.25, 2.0);
 
-	return 0;
+    return 0;
 }
