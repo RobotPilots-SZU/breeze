c2421bbc80daa6eedde0559e62d044bad49920bc
qianchen36 <xiang@morthine.com>
2025-08-09 03:02:42 +0800

    scripts: fix errors in env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,5 +4,5 @@ source ./.venv/bin/activate
 
 source ./breeze-sdk/zephyr/zephyr-env.sh
 
-CUR_PATH=$(dirname $(readlink -f "$0"))
+CUR_PATH=$(dirname "$(readlink -f \"$0\")")
 export ZEPHYR_SDK_INSTALL_DIR="$CUR_PATH/breeze-sdk/toolchain"
